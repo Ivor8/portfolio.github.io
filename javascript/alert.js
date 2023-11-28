19aa1fbfc735fdaf7eaa8502a416e642eaf6ba3a
Ivor8 <nirttech@gmail.com>
2023-11-28 20:02:01 +0100

    first commit

---
+++ javascript/alert.js
@@ -0,0 +1,3 @@
+function show(){
+    alert("We are sorry this form is not functional at the moment. you can cantact Ivor-Dev8 on social media via the links provided. Thaks for understanding");
+}
---
+++ javascript/togglenav.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.togglemenu')
+    sidebar.style.display = 'flex'
+}
+const hideSidebar  = () =>{
+    const sidebar = document.querySelector('.togglemenu')
+    sidebar.style.display = 'none'
+}
