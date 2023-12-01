6cd73a049903531cca57f71fc197a258f4398175
Ivor8 <nirttech@gmail.com>
2023-12-01 18:30:28 +0100

    correct spellings

--- javascript/alert.js
+++ javascript/alert.js
@@ -1,3 +1,3 @@
 function show(){
-    alert("We are sorry this form is not functional at the moment. you can cantact Ivor-Dev8 on social media via the links provided. Thaks for understanding");
+    alert("We are sorry! This form is not functional at the moment. you can cantact Ivor-Dev8 on social media via the links provided. Thanks for understanding");
 }
